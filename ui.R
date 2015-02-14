aea2ca60cb33520c69c649d54c5bf07a02577dfe
David Li <admin@imbatech.com>
2015-02-14 13:33:12 +0800

    update doc

--- ui.R
+++ ui.R
@@ -16,7 +16,7 @@ shinyUI(pageWithSidebar(
                 h2('Coefficient. Outcome is mpg'),
                 verbatimTextOutput("oid1"),
                 plotOutput('plot'),
-                h4('This application is used to caculate coefficients from linear model whose outcome is mpg, predictor is cyl, hp or am.'),
+                h4('This application is used to caculate coefficients from linear model whose outcome is mpg, predictor is cyl, hp or wt.'),
                 h4('1. Select predictor on the side bar'),
                 h4('2. If prefer to draw linear regression model, select "Draw Lm line"'),
                 h4('3. The result will display in the main panel. Coefficients start with intercept, followed by slope. Plot is based on selected predictor as x axis and mpg as y axis.')
